22b785846c8eac1e387c4c7ce7f8f1345c6b6fa2
Rafi'u Mahdaviqia <rafi'u@polinema.ac.id>
2024-09-02 10:53:04 +0700

    Pertemuan 2 - Percobaan 1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,10 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
         <title>PWL 2024</title>
-
-       
     </head>
     <body>
        <h2>Pewmweb Lanjut 2024</h2>
